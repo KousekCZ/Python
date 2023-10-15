c61fc9da1dd883d62638cd2ad3ae285403baf5eb
KousekCZ <105644565+KousekCZ@users.noreply.github.com>
2023-10-15 17:16:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ let websocket = null;
 let reconnecting = false;
 const messageInput = document.getElementById("message");
 const connectionStatus = document.getElementById("connection-status");
+let userName;
 
-// Přidáme proměnnou pro uživatelovo jméno
-let userName = prompt("Zadejte sve jmeno:");
+// Funkce pro zadání jména uživatele
+async function getUserName() {
+    while (!userName) {
+        userName = prompt("Zadejte sve jmeno:");
+    }
+}
+
+// Zavolání asynchronní funkce pro zadání jména uživatele
+getUserName()
+    .then(() => createWebSocket())
+    .catch((error) => console.error(error));
 
 function createWebSocket() {
     if (reconnecting) {
         const messagesDiv = document.getElementById("messages");
-        messagesDiv.innerHTML = "<p>Odpojen. Obnovuji spojeni...</p>" + messagesDiv.innerHTML;
+        messagesDiv.innerHTML = "<p>Odpojen. Obnovuji spojení...</p>" + messagesDiv.innerHTML;
     }
 
-    websocket = new WebSocket("wss://wa-websockets.onrender.com"); //ws://localhost:8080
+    websocket = new WebSocket("ws://localhost:8080");
     reconnecting = true;
 
     websocket.onmessage = function (event) {
         const messagesDiv = document.getElementById("messages");
         const message = event.data;
-
         messagesDiv.innerHTML = `<p>${message}</p>` + messagesDiv.innerHTML;
     };
 
@@ -39,8 +48,6 @@ function createWebSocket() {
         connectionStatus.style.color = "green";
     }
 }
-
-createWebSocket();
 
 document.getElementById("send").addEventListener("click", function () {
     sendMessage();
