51c01f665561e8c10b4de16a550b5355dfa5baa3
KousekCZ <105644565+KousekCZ@users.noreply.github.com>
2023-10-15 16:24:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import websockets
 from datetime import datetime, timedelta
 
 connected = {}  # Slovník pro ukládání připojených klientů
+banned_users = set()  # Množina pro ukládání banovaných uživatelů
+ip_to_client = {}  # Slovník pro mapování IP adres na klienty
+ban_duration = 10  # Doba banování v sekundách
 
 
 # Funkce pro zpracování zpráv
 async def websocket_handler(websocket, path):
     client_id = len(connected) + 1  # Přiřazení jedinečného ID klientovi
     connected[client_id] = websocket  # Uložení klienta do slovníku
-
     user_agent = websocket.request_headers.get('User-Agent', 'Console')
 
     try:
@@ -20,12 +22,22 @@ async def websocket_handler(websocket, path):
 
             print(f"Client {client_id} ({user_agent}): {message}")
 
+            if client_id in banned_users:
+                await websocket.send("Jste banován, nelze posílat zprávy.")
+                # Pokud je klient banovaný, odpojíme ho
+                await websocket.close()
+                continue
+
             if "Rum" in message:
                 # Banování klienta
-                await websocket.send("Byl jste zabanován za použití zakázaného slova 'Rum'.")
+                banned_users.add(client_id)  # Přidání klienta do seznamu banovaných
+                await websocket.send("Byl jste banován za použití zakázaného slova 'Rum'. Budete odbanován za 10s")
                 await websocket.close()
-                await asyncio.sleep(10)
-                del connected[client_id]  # Odstraníme klienta ze seznamu
+                ip_address = websocket.remote_address[0]  # Získání IP adresy klienta
+                ip_to_client[ip_address] = client_id  # Mapování IP adresy na klienta
+                await asyncio.sleep(ban_duration)  # Počkej 10 sekund
+                banned_users.discard(client_id)
+                ip_to_client.pop(ip_address, None)  # Odstranění z mapování IP adresy na klienta
 
             # Odeslání zprávy všem klientům s časem
             for client in connected.values():
@@ -38,6 +50,7 @@ async def websocket_handler(websocket, path):
     finally:
         # Smažeme odpojeného klienta z evidovaných
         del connected[client_id]
+        banned_users.discard(client_id)
 
 
 # Funkce pro spuštění WebSocket serveru
